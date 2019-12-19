100985cd0feea67964f283b557699bda877f202d
e <378098663@qq.com>
2019-12-19 15:54:06 +0800

    格式修改

--- src/main/java/com/wz/emptyframe/controller/favorites/FavoritesGroupController.java
+++ src/main/java/com/wz/emptyframe/controller/favorites/FavoritesGroupController.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
-
 /**
  * <p>
  * 
@@ -27,39 +25,39 @@ public class FavoritesGroupController {
 
     @Autowired
     @Qualifier("favoritesGroupServiceImpl")
-    private FavoritesGroupService favoritesGroupService;
+    private FavoritesGroupService defaultService;
 
     @GetMapping("/get")
     @ApiOperation(value = "获取详情")
     private Object get(String id) {
-        return WebDTO.success(favoritesGroupService.getById(id));
+        return WebDTO.success(defaultService.getById(id));
     }
 
     @PostMapping("/add")
     @ApiOperation(value = "添加")
     public Object add(@RequestBody FavoritesGroup favoritesGroup) {
-        boolean b = favoritesGroupService.save(favoritesGroup);
+        boolean b = defaultService.save(favoritesGroup);
         return b?WebDTO.success():WebDTO.faliure(null,null);
     }
 
     @DeleteMapping("/delete")
     @ApiOperation(value = "删除")
     public Object delete(String id) {
-        boolean b = favoritesGroupService.removeById(id);
+        boolean b = defaultService.removeById(id);
         return b?WebDTO.success():WebDTO.faliure(null,null);
     }
 
     @PostMapping("/update")
     @ApiOperation(value = "修改")
     public Object update(@RequestBody FavoritesGroup favoritesGroup) {
-        boolean b = favoritesGroupService.updateById(favoritesGroup);
+        boolean b = defaultService.updateById(favoritesGroup);
         return b?WebDTO.success():WebDTO.faliure(null,null);
     }
 
     @PostMapping("/list")
     @ApiOperation(value = "获取列表")
     public Object list() {
-        return WebDTO.success(favoritesGroupService.list());
+        return WebDTO.success(defaultService.list());
     }
 
 }
